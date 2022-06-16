5e8adf6d4bd8179a74b422b321048fee17217893
Eric Moore <emoore501@gmail.com>
2022-06-15 21:53:59 -0400

    test pass

--- src/components/Home.test.js
+++ src/components/Home.test.js
@@ -10,3 +10,4 @@ import Home from "./Home";
     })
 
    })    
+   
--- src/components/Track.test.js
+++ src/components/Track.test.js
@@ -1,28 +1,32 @@
-import { toHaveTextContent } from '@testing-library/jest-dom/dist/matchers';
-import {fireEvent, render, screen} from '@testing-library/react';
+//import { toHaveTextContent } from '@testing-library/jest-dom/dist/matchers';
+
+
+import {render,fireEvent, screen} from '@testing-library/react';
 import Track from "./Track";
 
 
 
 
-describe("<Track />", () => {
+describe("<Track />", () =>  {
    it("should render track form with 2 buttons", async () => {
        render(<Track />);
        const buttonList = await screen.findAllByRole("button");
        expect(buttonList).toHaveLength(2);
    });
 
-   /*it("It should allow letters to be inputted", () => {
-       const {input} = updateUserWaterInput()
-       expect(input.value).toBe('')
-       fireEvent.change(input, {target: {value: 'test'}})
-       expect(input.value).toBe('')*/
-       
+   it("It should display fruit amount on track page", async () => {
+    render(<Track />)
+   
+    //Click button
+    fireEvent.click(screen.getByText('Eat'))
+    const items = screen.findByLabelText("btn btn-primary")
+    expect(items).toMatchObject(items)
+   
    })
 
    const Button = ({onClick, children}) => (
     <button onClick={onClick}>{children}</button>
-  )
+   )
   
   it('calls onClick prop when clicked', () => {
     const handleClick = jest.fn()
@@ -33,16 +37,12 @@ describe("<Track />", () => {
   })
 
 
+ 
+
+
+
   // it("should save water consumed when drink button is pressed")
   //it("should save fruit consumed when eat button is pressed")
   //it("should display water amount on track page")
    //it("should display fruit amount on track page")
-  
-
-
-
-  
-   
-
-
-
+})
